b855d846347a8b749ea2274db7e39205b72bd0dd
iman370 <imaniqbal@live.co.uk>
2022-06-15 19:56:10 +0100

    Created the folders for the other website aspects

--- klabber/settings.py
+++ klabber/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'events.apps.EventsConfig',
+    'friends.apps.FriendsConfig',
+    'klabs.apps.KlabsConfig',
     'rest_framework',
     'rest_framework_jwt',
 ]
