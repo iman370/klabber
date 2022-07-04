56c7f089abaf54aecdc96297e2e44edb8e8e35b8
iman370 <imaniqbal@live.co.uk>
2022-07-04 22:24:16 +0100

    CreateKlab page designed a bit more

--- frontend/src/pages/CreateKlab.js
+++ frontend/src/pages/CreateKlab.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function CreateKlab() {
     let navigate = useNavigate();
@@ -12,20 +13,35 @@ function CreateKlab() {
     
 
     return (
-        <div id="create-klab-content">
-            <h1>username</h1>
-            <div className='divider' />
-            <h1>date</h1>
-            <div className='divider' />
-            <h1>time</h1>
-            <div className='divider' />
-            <h1>place</h1>
-            <div className='divider' />
-            <h1>description</h1>
-            <div className='divider' />
-            <h1>max spaces</h1>
-            <div className='divider' />
-            <Button onClick={() => navigate('../home',{state:{username,first_name,email}})}>Back</Button>
+        <div id="create-klab-container">
+            <div id="create-klab-left">
+                <h1>What is this klab for?</h1>
+                <div className='divider' />
+                <h1>What date will this be?</h1>
+                <div className='divider' />
+                <h1>Time?</h1>
+                <div className='divider' />
+                <h1>Where will it be held?</h1>
+                <div className='divider' />
+                <h1>How many people can join?</h1>
+                <p>(Excluding you)</p>
+                <TextField
+                    id="filled-number"
+                    label="Maximum Spaces"
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="filled"
+                />
+                <div className='divider' />
+                <h1>Do you want to invite anyone specific?</h1>
+                <div className='divider' />
+            </div>
+            <div id="create-klab-right">
+                <h1>test</h1>
+                <Button onClick={() => navigate('../home',{state:{username,first_name,email}})}>Back</Button>
+            </div>
         </div>
     )
 }
