e3574db16915518b8b60afeb4004e1ae422fe66e
iman370 <imaniqbal@live.co.uk>
2022-07-05 13:25:50 +0100

    Finally centered things in create klab page

--- frontend/src/pages/CreateKlab.js
+++ frontend/src/pages/CreateKlab.js
@@ -15,28 +15,30 @@ function CreateKlab() {
     return (
         <div id="create-klab-container">
             <div id="create-klab-left">
-                <h1>What is this klab for?</h1>
-                <div className='divider' />
-                <h1>What date will this be?</h1>
-                <div className='divider' />
-                <h1>Time?</h1>
-                <div className='divider' />
-                <h1>Where will it be held?</h1>
-                <div className='divider' />
-                <h1>How many people can join?</h1>
-                <p>(Excluding you)</p>
-                <TextField
-                    id="filled-number"
-                    label="Maximum Spaces"
-                    type="number"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    variant="filled"
-                />
-                <div className='divider' />
-                <h1>Do you want to invite anyone specific?</h1>
-                <div className='divider' />
+                <div id="left-container">
+                    <h1>What is this klab for?</h1>
+                    <div className='divider' />
+                    <h1>What date will this be?</h1>
+                    <div className='divider' />
+                    <h1>Time?</h1>
+                    <div className='divider' />
+                    <h1>Where will it be held?</h1>
+                    <div className='divider' />
+                    <h1>How many people can join?</h1>
+                    <p>(Excluding you)</p>
+                    <TextField
+                        id="filled-number"
+                        label="Maximum Spaces"
+                        type="number"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        variant="filled"
+                    />
+                    <div className='divider' />
+                    <h1>Do you want to invite anyone specific?</h1>
+                    <div className='divider' />
+                </div>
             </div>
             <div id="create-klab-right">
                 <h1>test</h1>
