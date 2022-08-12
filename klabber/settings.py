034b91b794e0fc7646805adfb6268464b50e4c87
iman370 <imaniqbal@live.co.uk>
2022-08-12 17:54:22 +0100

    Setting up for heroku

--- klabber/settings.py
+++ klabber/settings.py
@@ -137,6 +137,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'frontend/build/static')]
 
+DISABLE_COLLECTSTATIC=1
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
